281fda0efa528d5cfe691a5941681d8c18908edf
oshadha97 <oshadha97@gmail.com>
2024-07-12 15:07:42 +0100

    Add files via upload

---
+++ Interview/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+// Display numbers 1 to 100 , if nmber is divisible by 3 display as Fizzy, 
+// if number divisible by 5 dissplay as Buzzy , if number divisible both 3 and 5 dispalay 
+//as Buzzy and Fizzy
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        Console.WriteLine("Fizz Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine("{0}", i);
+    }
+}
